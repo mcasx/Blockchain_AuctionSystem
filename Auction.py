da6605f3f83b4ad3f6f30e26be1f16f511bd19e2
David <davidmoreiraalmeida@hotmail.com>
2018-10-10 15:52:20 +0100

    auction_repository barebones done. Need testing

--- Auction.py
+++ Auction.py
@@ -1,9 +1,34 @@
+from hashlib import sha256
+from Bid import Bid
+from datetime import datetime, timedelta
+import threading
+
 class Auction:
-    def __init__(self, name, serialNumber, timeLimit, description, auctionType, creator):
+    
+    def __init__(self, name, serial_number, time_limit, description, auctionType, creator, rules):
         self.name = name
-        self.serialNumber = serialNumber
-        self.timeLimit = timeLimit
+        self.serial_number = serial_number
+        self.time_limit = time_limit
         self.description = description
         self.auctionType = auctionType
         self.creator = creator
-        self.bids = [] 
+        self.bids = []
+        self.rules = rules
+        self.state = "Open"
+        now = datetime.now()
+        if now > time_limit:
+            self.close()
+        else:
+            delay = (time_limit - now).total_seconds()
+            threading.Timer(delay, self.close).start()
+
+        
+    def add_bid(self, user, value):
+        if self.rules(self, user, value):
+            self.bids.append(Bid(user, value, None) if not len(self.bids) else Bid(user, value, sha256(self.bids[-1])))
+            return True
+        else:
+            return False
+
+    def close(self):
+        self.state = "Closed"
--- auction_repository.py
+++ auction_repository.py
@@ -1,5 +1,7 @@
-#import auction
-from flask import Flask
+from Auction import Auction
+from flask import Flask,request
+import pickle
+import datetime
 
 app = Flask(__name__)
 auctions = []
@@ -8,6 +10,40 @@ auctions = []
 def hello():
     return "Hey"
 
+@app.route("/create_auction", methods=['GET', 'POST'])
+def create_auction():
+    auction = pickle.loads(request.form['auction'])
+    auctions.append(auction)
+    return "Auction Created"
+
+@app.route("/bid", methods=['POST'])
+def bid():
+    serial_number = request.form['serial_number']
+    print(serial_number)
+    auction = get_auction(serial_number)
+    if auction == None: return "Auction does not exist"
+    if auction.state == "Closed": return "Bid refused"
+    user = request.form['user']
+    value = request.form['value']
+    
+    return "Bid added" if auction.add_bid(user, value) else "Bid refused"
+
+@app.route("/close_auction", methods=['POST'])
+def close_auction():
+    serial_number = request.form['serial_number']
+    auction = get_auction(serial_number)
+    if auction == None: return "Auction does not exist"
+    if auction.state == "Closed": return "Auction already closed"
+    auction.state = "Closed"
+    return "Auction closed"
+
+
+def get_auction(serial_number):
+    for a in auctions:
+        if a.serial_number == serial_number: return a.serial_number
+    return None
+
 
 if __name__ == "__main__":
     app.run()
+
